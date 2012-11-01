1be4414136702d92c3cee425555d0f70690fa2bf
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-11-01 22:27:42 +0100

    Adding licence information

--- include/user_object_analyzer_db.h
+++ include/user_object_analyzer_db.h
@@ -1,3 +1,23 @@
+/*
+    This file is part of osm_diff_analyzer_user_object, Openstreetmap
+    diff analyzer based on CPP diff representation. It's aim is to survey
+    objects edited by parametered used and to generate an alert in case of 
+    edition on these objects
+    Copyright (C) 2012  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #ifndef _USER_OBJECT_ANALYZER_DB_H_
 #define _USER_OBJECT_ANALYZER_DB_H_
 
